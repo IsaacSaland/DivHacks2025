8ac60d3472caf9d7c1e39457681952fd15751a08
Nicholas Assaderaghi <nra2130@columbia.edu>
2025-10-04 17:31:31 -0400

    tab names

--- snap-chef/src/App.tsx
+++ snap-chef/src/App.tsx
@@ -10,8 +10,8 @@ import {
   setupIonicReact
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import { ellipse, square, triangle } from 'ionicons/icons';
-import Tab1 from './pages/Tab1';
+import { ellipse, square, triangle, camera, leaf, fastFood } from 'ionicons/icons';
+import Scanner from './pages/Tab1';
 import Tab2 from './pages/Tab2';
 import Tab3 from './pages/Tab3';
 
@@ -53,7 +53,7 @@ const App: React.FC = () => (
       <IonTabs>
         <IonRouterOutlet>
           <Route exact path="/tab1">
-            <Tab1 />
+            <Scanner />
           </Route>
           <Route exact path="/tab2">
             <Tab2 />
@@ -67,16 +67,16 @@ const App: React.FC = () => (
         </IonRouterOutlet>
         <IonTabBar slot="bottom">
           <IonTabButton tab="tab1" href="/tab1">
-            <IonIcon aria-hidden="true" icon={triangle} />
-            <IonLabel>Tab 1</IonLabel>
+            <IonIcon aria-hidden="true" icon={camera} />
+            <IonLabel>Scan</IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab2" href="/tab2">
-            <IonIcon aria-hidden="true" icon={ellipse} />
-            <IonLabel>Tab 2</IonLabel>
+            <IonIcon aria-hidden="true" icon={fastFood} />
+            <IonLabel>Plan</IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab3" href="/tab3">
-            <IonIcon aria-hidden="true" icon={square} />
-            <IonLabel>Tab 3</IonLabel>
+            <IonIcon aria-hidden="true" icon={leaf} />
+            <IonLabel>Compost</IonLabel>
           </IonTabButton>
         </IonTabBar>
       </IonTabs>
